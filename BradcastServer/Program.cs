0c2a7608bfac8d646a63bed19def8ad932612a84
HerasymovIvan <gerasymov.ivan@gmail.com>
2025-10-04 12:52:19 +0300

    Add project files.

---
+++ BradcastServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+
+//Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+//var tcpListner = new TcpClient(System.Net.IPAddress.Any,11000);
+
+var udpClient = new UdpClient();
+
+// acces sending broadcast messages
+udpClient.EnableBroadcast = true;
+
+var broadcastAddres = System.Net.IPAddress.Broadcast; 
+var port = 5000;
+
+var enpdoint = new System.Net.IPEndPoint(broadcastAddres, port);
+
+while (true)
+{
+    var message = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+    var data=System.Text.Encoding.UTF8.GetBytes(message);
+
+    await udpClient.SendAsync(data, data.Length, enpdoint);
+    Console.WriteLine(message);
+    await Task.Delay(5000);
+
+}
---
+++ BroadcastClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var port = 5000;
+
+var udpClient=new UdpClient(port);
+
+var endpoint=new IPEndPoint(IPAddress.Any, port);  
+
+while (true)
+{
+    byte[] receivedBytes = udpClient.Receive(ref endpoint);
+
+    var message = System.Text.Encoding.UTF8.GetString(receivedBytes);
+
+    Console.WriteLine(message);
+}
